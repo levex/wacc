f8e31c6239aa02ae2934f2b362ec86888947dcf4
Daniel Zvara <dz2615@ray21.doc.ic.ac.uk>
2016-10-25 20:53:27 +0100

    Semantics/Semantics.hs: semantic checker implementation module
    Signed-off-by: Daniel Zvara <dz2615@ray21.doc.ic.ac.uk>

---
+++ src/WACC/Semantics/Semantics.hs
@@ -0,0 +1,44 @@
+module WACC.Semantics.Semantics where
+
+import      WACC.Semantics.Types
+import      WACC.Parser.Types
+
+decreaseScope :: [SymbolTable] -> [SymbolTable]
+decreaseScope []
+  = []
+decreaseScope (SymbolTable ((Symbol _ _) : st) : stt)
+  = stt
+decreaseScope ((SymbolTable []) : stt)
+  = stt
+decreaseScope ((SymbolTable st) : stt)
+  = ((SymbolTable (decreaseScope st)) : stt)
+decreaseScope st
+  = st
+
+increaseScope :: [SymbolTable] -> [SymbolTable]
+increaseScope []
+  = [SymbolTable []]
+increaseScope ((Symbol i t) : stt)
+  = [SymbolTable [], Symbol i t] ++ stt
+increaseScope ((SymbolTable st) : stt)
+  = (SymbolTable (increaseScope st)) : stt
+
+addSymbol :: SymbolTable -> [SymbolTable] -> [SymbolTable]
+addSymbol s ((SymbolTable st) : stt)
+  = ((SymbolTable (addSymbol s st)) : stt)
+addSymbol s st
+  = (s : st)
+
+isDefined :: SymbolTable -> [SymbolTable] -> Bool
+isDefined s st
+  = s `elem` (concatMap flatten st)
+
+flatten :: SymbolTable -> [SymbolTable]
+flatten (SymbolTable st)
+  = concatMap flatten st
+flatten s
+  = [s]
+
+semanticChecker :: AnnotatedProgram -> Program
+semanticChecker ap
+  = []
