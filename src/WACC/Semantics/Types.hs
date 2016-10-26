681d154e0a9cabc1b34e53c07da6a31b3aa49428
Daniel Zvara <dan.zvara@gmail.com>
2016-10-26 11:49:19 +0100

    semantic/types: added SemanticError
    Signed-off-by: Daniel Zvara <dan.zvara@gmail.com>

--- src/WACC/Semantics/Types.hs
+++ src/WACC/Semantics/Types.hs
@@ -2,8 +2,14 @@ module WACC.Semantics.Types where
 
 import          WACC.Parser.Types
 
+type Row = Int
+type Column = Int
+
 data SymbolTable
   = Symbol Identifier Type
   | SymbolTable [SymbolTable]
   deriving (Eq, Show)
 
+data SemanticError = SemanticError Position String
+
+data Position = Position Row Column
