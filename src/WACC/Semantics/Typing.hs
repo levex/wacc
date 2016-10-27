e263ebe0fad19f5868ea18cd48ff8850c0245751
Daniel Zvara <dan.zvara@gmail.com>
2016-10-28 00:37:23 +0100

    semantics/typing: fixed getType
    Signed-off-by: Daniel Zvara <dan.zvara@gmail.com>

--- src/WACC/Semantics/Typing.hs
+++ src/WACC/Semantics/Typing.hs
@@ -6,6 +6,7 @@ import WACC.Parser.Types
 import WACC.Parser.Primitives
 import WACC.Semantics.Semantics
 import WACC.Semantics.Types
+import WACC.Semantics.Primitives
 
 unOpTypes
   = [ (Not, (TBool, TBool))
@@ -35,42 +36,42 @@ binAppTypes
     , (NEq, (TInt, TInt, TBool))
     ]
 
-getLiteralType :: Literal -> SymbolTable -> Maybe Type
-getLiteralType (CHAR _) _ = Just TChar
-getLiteralType (INT _) _  = Just TInt
-getLiteralType (BOOL _) _ = Just TBool
-getLiteralType (STR _) _  = Just TString
-getLiteralType (ARRAY (e : _)) st
-  = case getType e st of
-      (Just t) -> Just (TArray t)
-      _        -> Nothing
+getLiteralType :: Literal -> SemanticChecker Type
+getLiteralType (CHAR _)        = return TChar
+getLiteralType (INT _)         = return TInt
+getLiteralType (BOOL _)        = return TBool
+getLiteralType (STR _)         = return TString
+getLiteralType (ARRAY (e : _)) = getType e
 
-deconstructArrayType :: Maybe Type -> Maybe Type
-deconstructArrayType (Just (TArray t)) = Just t
-deconstructArrayType t                 = Nothing
+deconstructArrayType :: Type -> SemanticChecker Type
+deconstructArrayType (TArray t)
+  = return t
+deconstructArrayType t
+  = semanticInvalid $ "Expected TArray, Found: " ++ (show t)
 
-getType :: Expr -> SymbolTable -> Maybe Type
-getType (Lit lit) st
-  = getLiteralType lit st
-getType (Ident id) st
-  = getTypeForId id st
-getType (ArrElem id _) st
-  = deconstructArrayType $ getType (Ident id) st
-getType (PairElem e id) st
-  = case getType (Ident id) st of
-      (Just (TPair f s)) -> Just (pairElem e f s)
-      _                  -> Nothing
+getType :: Expr -> SemanticChecker Type
+getType (Lit lit)
+  = getLiteralType lit
+getType (Ident id)
+  = identLookup id
+getType (ArrElem id _)
+  = getType (Ident id) >>= deconstructArrayType
+getType (PairElem e id) = do
+  (TPair f s) <- getType (Ident id)
+  return (pairElem e f s)
   where
     pairElem Fst f _ = f
     pairElem Snd _ s = s
-getType (UnApp op _) _
-  = (maybe Nothing (\(_, x) -> (Just x))) . lookup op $ unOpTypes
-getType (BinApp op _ _) _
-  = (maybe Nothing (\(_,_,x) -> (Just x))) . lookup op $ binAppTypes
-getType (FunCall id _) st
-  = getTypeForId id st
-getType (NewPair e1 e2) st
-  = Just (TPair t1 t2) -- FIXME: pair<T1,T2>
+getType (UnApp op _)
+  = (maybe (semanticInvalid "undefined operation") (return.fst)) . lookup op $ unOpTypes
+getType (BinApp op _ _)
+  = (maybe (semanticInvalid "undefined operation") tripleFirst) . lookup op $ binAppTypes
   where
-    (Just t1) = getType e1 st
-    (Just t2) = getType e2 st
+    tripleFirst (x,_,_) = return x
+getType (FunCall id _)
+  = identLookup id
+getType (NewPair e1 e2)    -- FIXME: pair<T1,T2>
+  = do
+      t1 <- getType e1
+      t2 <- getType e2
+      return (TPair t1 t2)
