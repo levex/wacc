0cdb23a1c50c42cd0fe4490e259b72a20cfce701
Emma Gospodinova <emma.gospodinova@gmail.com>
2016-11-21 21:30:32 +0000

    codegen: fixed string escaping

--- src/WACC/CodeGen/Strings.hs
+++ src/WACC/CodeGen/Strings.hs
@@ -12,34 +12,34 @@ emitString :: Identifier -> String -> CodeGenerator String
 emitString id str
   = return $ concat ["  ", id, ":\n",
                      "    ", ".word ", show . length $ str, "\n",
-                     "    ", ".asciz ", show str, "\n"]
+                     "    ", ".asciz ", str, "\n"]
 
 emitStringLiteral :: Instruction -> CodeGenerator String
 emitStringLiteral (Special (StringLit id str))
-  = emitString id str
+  = emitString id (show str)
 emitStringLiteral _
   = return []
 
 emitBuiltinString :: Identifier -> CodeGenerator String
 emitBuiltinString "__builtin_Read_TChar"
-  = emitString "__builtin_fmt_read_char" " %c\0"
+  = emitString "__builtin_fmt_read_char" " \"%c\\0\""
 
 emitBuiltinString "__builtin_Read_TInt"
-  = emitString "__builtin_fmt_read_int" " %d\0"
+  = emitString "__builtin_fmt_read_int" " \"%d\\0\""
 
 emitBuiltinString "__builtin_Print_TInt"
-  = emitString "__builtin_fmt_int" "%d"
+  = emitString "__builtin_fmt_int" "\"%d\""
 
 emitBuiltinString "__builtin_Print_TBool"
   = liftM2 (++)
-      (emitString "__builtin_str_true" "true")
-      (emitString "__builtin_str_false" "false")
+      (emitString "__builtin_str_true" "\"true\"")
+      (emitString "__builtin_str_false" "\"false\"")
 
 emitBuiltinString "__builtin_Print_TString"
-  = emitString "__builtin_fmt_string" "%.*s\0"
+  = emitString "__builtin_fmt_string" "\"%.*s\\0\""
 
 emitBuiltinString "__builtin_PrintLn"
-  = emitString "__builtin_str_newline" "\0"
+  = emitString "__builtin_str_newline" "\"\\0\""
 
 emitBuiltinString _
   = return []
