773e0f9f36b27a3d4be41ac2170c543cd657cfe9
Emma Gospodinova <emma.gospodinova@gmail.com>
2016-11-24 19:00:46 +0000

    emitarm: replaced redundant nameForReg function with show

--- src/WACC/CodeGen/ARM/Emit.hs
+++ src/WACC/CodeGen/ARM/Emit.hs
@@ -40,9 +40,6 @@ opTable = [ (AddOp,  "add")
           , (XorOp,  "eor")
           , (AndOp,  "and")]
 
-nameForReg :: Register -> String
-nameForReg = show
-
 genModifier :: Eq a => [(a, String)] -> a -> String -> String
 genModifier t = flip (++) . fromJust . flip lookup t
 
@@ -70,56 +67,56 @@ instance Emit Instruction where
   emit (Load c m rt op1 plus op2)
     = [genCond c (genSize m "ldr"), " "] ++
         case op1 of
-          Imm i   -> [nameForReg rt, ", =", show i, "\n"]
-          Label l -> [nameForReg rt, ", =", l, "\n"]
+          Imm i   -> [show rt, ", =", show i, "\n"]
+          Label l -> [show rt, ", =", l, "\n"]
           Reg rn  -> case op2 of
-            Reg rm -> [nameForReg rt, ", [", nameForReg rn, ", ",
-                        if plus then "" else "-", nameForReg rm, "]\n"]
-            Imm 0  -> [nameForReg rt, ", [", nameForReg rn, "]\n"]
-            Imm i2 -> [nameForReg rt, ", [", nameForReg rn, ", ",
+            Reg rm -> [show rt, ", [", show rn, ", ",
+                        if plus then "" else "-", show rm, "]\n"]
+            Imm 0  -> [show rt, ", [", show rn, "]\n"]
+            Imm i2 -> [show rt, ", [", show rn, ", ",
                                                   "#", show i2, "]\n"]
 
   emit (Move c rt op1)
     = [genCond c "mov", " "] ++
         case op1 of
-          Imm i  -> [nameForReg rt, ", #", show i, "\n"]
-          Reg rn -> [nameForReg rt, ", ", nameForReg rn, "\n"]
+          Imm i  -> [show rt, ", #", show i, "\n"]
+          Reg rn -> [show rt, ", ", show rn, "\n"]
 
   emit (Shift c rt rn st i)
     = [genCond c (map toLower (show st)), " ",
-        nameForReg rt, ", ", nameForReg rn, ", #", show i, "\n"]
+        show rt, ", ", show rn, ", #", show i, "\n"]
 
   emit (Push c rs)
     = [genCond c "push", " {",
-        intercalate ", " $ map nameForReg (sort rs), "}\n"]
+        intercalate ", " $ map show (sort rs), "}\n"]
 
   emit (Pop c rs)
     = [genCond c "pop", " {",
-        intercalate ", " $ map nameForReg (sort rs), "}\n"]
+        intercalate ", " $ map show (sort rs), "}\n"]
 
   emit (Branch c op1)
     = case op1 of
         Label lab -> [genCond c "b", " ", lab, "\n"]
-        Reg rt    -> [genCond c "bx", " ", nameForReg rt, "\n"]
+        Reg rt    -> [genCond c "bx", " ", show rt, "\n"]
 
   emit (BranchLink c op1)
     = case op1 of
         Label lab -> [genCond c "bl", " ", lab, "\n"]
-        Reg rt    -> [genCond c "blx", " ", nameForReg rt, "\n"]
+        Reg rt    -> [genCond c "blx", " ", show rt, "\n"]
 
   emit (Compare c rt op1) = do
     [genCond c "cmp", " "] ++
       case op1 of
-        Reg rn -> [nameForReg rt, ", ", nameForReg rn, "\n"]
-        Imm i  -> [nameForReg rt, ", #", show i, "\n"]
+        Reg rn -> [show rt, ", ", show rn, "\n"]
+        Imm i  -> [show rt, ", #", show i, "\n"]
 
   emit (Store c m rt rn plus op2) = do
     [genCond c (genSize m "str"), " "] ++
       case op2 of
-        Reg rm -> [nameForReg rt, ", [", nameForReg rn, ", ",
-                    if plus then "" else "- ", nameForReg rm, "]\n"]
-        Imm 0  -> [nameForReg rt, ", [", nameForReg rn, "]\n"]
-        Imm i  -> [nameForReg rt, ", [", nameForReg rn, ", #", show i, "]\n"]
+        Reg rm -> [show rt, ", [", show rn, ", ",
+                    if plus then "" else "- ", show rm, "]\n"]
+        Imm 0  -> [show rt, ", [", show rn, "]\n"]
+        Imm i  -> [show rt, ", [", show rn, ", #", show i, "]\n"]
 
   emit (Op c ModOp rt rn op1) = concatMap emit
       [ Push c [r0, r1]
@@ -140,8 +137,8 @@ instance Emit Instruction where
   emit (Op c op rt rn op1)
     = [genCond c (fromJust $ lookup op opTable), " "] ++
       case op1 of
-        Reg rm -> [intercalate ", " $ map nameForReg [rt, rn, rm],"\n"]
-        Imm i  -> [nameForReg rt, ", ", nameForReg rn, ", #", show i,"\n"]
+        Reg rm -> [intercalate ", " $ map show [rt, rn, rm],"\n"]
+        Imm i  -> [show rt, ", ", show rn, ", #", show i,"\n"]
 
   emit (SWI i)
     = ["swi #", show i, "\n"]
